ab932f3f9a753f7fdc6cf37039f630cd7c6b5028
valainis.nauris@gmail.com <73598402+NaurisV7@users.noreply.github.com>
2021-06-09 19:10:51 +0300

    completed till crown in main part

---
+++ script.js
@@ -0,0 +1,26 @@
+let container = document.getElementById("container");
+let input = document.getElementById("input");
+let checkbox = document.getElementById("checkbox");
+let button = document.getElementById("button");
+
+
+button.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (!input.value){
+        container.innerHTML = "<p>Email address is required</p>"
+    } else if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input.value)) {
+        container.innerHTML = "<p>Please provide a valid e-mail address</p>"
+    } else if (!checkbox.checked) {
+        container.innerHTML = "<p>You must accept the terms and conditions</p>"
+    } else if (input.value.endsWith(".co")) {
+        container.innerHTML = "<p>We are not accepting subscriptions from Colombia emails</p>.";
+    } else {
+        container.innerHTML = "";
+        document.getElementById("main").innerHTML = "<h1>sanaca</h1>";
+    }
+})
+
+
+
+
+//container.innerHTML = "<p>We are not accepting subscriptions from Colombia emails.</p>"
